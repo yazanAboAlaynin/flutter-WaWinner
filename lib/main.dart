721b1012299c45fa20bfcce38c2bb5f8c4168c1e
YazanAboAlaynin <yazanmohned@gmail.com>
2020-10-18 16:41:36 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wawinner/screens/auth/LoginPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: WaWinner(),
+    );
+  }
+}
+
+class WaWinner extends StatefulWidget {
+  @override
+  _WaWinnerState createState() => _WaWinnerState();
+}
+
+class _WaWinnerState extends State<WaWinner> {
+  @override
+  Widget build(BuildContext context) {
+    return LoginPage();
+  }
+}
---
+++ lib/screens/auth/LoginPage.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final sizeAware = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          width: sizeAware.width,
+          height: sizeAware.height,
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome',
+                style: TextStyle(
+                    color: Color.fromRGBO(53, 9, 100, 1.0),
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: sizeAware.height * 0.01,
+              ),
+              Text(
+                'Sign in to continue',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: sizeAware.height * 0.01,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                ),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                ),
+              ),
+              SizedBox(
+                height: sizeAware.height * 0.02,
+              ),
+              Container(
+                width: sizeAware.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    MaterialButton(
+                      onPressed: () {},
+                      height: 45.0,
+                      minWidth: sizeAware.width * 0.85,
+                      shape: ContinuousRectangleBorder(
+                        borderRadius: BorderRadius.horizontal(
+                            left: Radius.circular(40),
+                            right: Radius.circular(40)),
+                      ),
+                      color: Color.fromRGBO(127, 25, 168, 1),
+                      textColor: Colors.white,
+                      child: new Text(
+                        'Login',
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                      splashColor: Color.fromRGBO(53, 9, 100, 1.0),
+                    ),
+                    SizedBox(
+                      height: sizeAware.height * 0.02,
+                    ),
+                    Text(
+                      'or',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                    SizedBox(
+                      height: sizeAware.height * 0.02,
+                    ),
+                    Container(
+                      width: sizeAware.width * 0.85,
+                      height: 45.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.horizontal(
+                          left: Radius.circular(40),
+                          right: Radius.circular(40),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Login with Facebook',
+                              style: TextStyle(
+                                  fontSize: 17,
+                                  color: Colors.grey[600],
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: sizeAware.height * 0.02,
+                    ),
+                    Text('Forgot Password'),
+                    SizedBox(
+                      height: sizeAware.height * 0.02,
+                    ),
+                    Text('Dont have account'),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: sizeAware.height * 0.04,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
